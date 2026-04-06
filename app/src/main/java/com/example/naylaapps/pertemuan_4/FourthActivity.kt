a0c290946756be3e55d0a17ee38cfb8122467e28
Nayla <nayla24si@mahasiswa.pcr.ac.id>
2026-04-06 08:11:46 +0700

    pengiriman data intent

--- app/src/main/java/com/example/naylaapps/pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/naylaapps/pertemuan_4/FourthActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.naylaapps.R
+import com.example.naylaapps.databinding.ActivityFourthBinding
 
 class FourthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
