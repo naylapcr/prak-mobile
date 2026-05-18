75f00f0f7e079a921f2653c514f93786ffba2874
Nayla <nayla24si@mahasiswa.pcr.ac.id>
2026-05-18 07:57:39 +0700

    p10 TabLayout

--- app/src/main/java/com/example/naylaapps/Home/HomeFragment.kt
+++ app/src/main/java/com/example/naylaapps/Home/HomeFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.edit
 import com.example.naylaapps.AuthActivity
+import com.example.naylaapps.Home.pertemuan_10.TenthActivity
 import com.example.naylaapps.Home.pertemuan_2.SecondActivity
 import com.example.naylaapps.Home.pertemuan_4.FourthActivity
 import com.example.naylaapps.Home.pertemuan_5.FifthActivity
@@ -86,6 +87,12 @@ class HomeFragment : Fragment() {
         binding.btnToNinth.setOnClickListener {
             val intent = Intent(requireContext(), NinthActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.btnToTenth.setOnClickListener {
+            val intent = Intent(requireContext(), TenthActivity::class.java)
+            startActivity(intent)
+
         }
         super.onViewCreated(view, savedInstanceState)
     }
